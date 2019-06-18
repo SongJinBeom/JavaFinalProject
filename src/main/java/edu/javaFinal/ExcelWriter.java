76d6cf4cdaaa48a87c3c76427c84d53b3e2ffbfa
SongJinBeom <cooo8@naver.com>
2019-06-18 14:39:42 +0900

    tt
    ttt

--- src/main/java/edu/javaFinal/ExcelWriter.java
+++ src/main/java/edu/javaFinal/ExcelWriter.java
@@ -1,6 +1,10 @@
 package edu.javaFinal;
 
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
@@ -8,19 +12,50 @@ public class ExcelWriter {
 	
 	
 	public static void writeAFile(ArrayList<String> lines, String targetFileName, int count) {
-		PrintWriter outputStream = null;
+		File file = new File(targetFileName+count+".csv");
 		try {
-			targetFileName = targetFileName + count + ".csv";
-			outputStream = new PrintWriter(targetFileName);
-		} catch (FileNotFoundException e) {
-			System.out.println("Error writing the file " + targetFileName);
-			System.exit(0);
+			if(!file.exists()) {
+				file.getParentFile().mkdir();
+				file.createNewFile();
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
-		for (String line : lines) {
-			System.out.println(line);
-			outputStream.println(line);
+		try {
+			BufferedWriter stream = new BufferedWriter(new FileWriter(file, true));
+			String line = "";
+			
+			for(String cell : lines) {
+				stream.write(cell);
+				stream.flush();
+			}
+			stream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		outputStream.close();
+		
 	}
+//	
+//	try {
+//		BufferedWriter stream = new BufferedWriter(new FileWriter(output, true));
+//		String oneLine = "";
+//		
+//		if(type == 0 && isFirst1) {
+//			isFirst1 = !isFirst1;
+//		}else if(type == 1 && isFirst2) {
+//			isFirst2 = !isFirst2;
+//		}
+//		for(String row : data) {
+//			oneLine = oneLine + row + ",";
+//		}
+//		System.out.println("oneLine: "+oneLine);
+//		stream.write(oneLine + "\n");
+//		stream.flush();
+//		stream.close();
+//	} catch(IOException e) {
+//		e.printStackTrace();
+//	}
 	
 }
+
